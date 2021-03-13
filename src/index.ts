4993a3e48967386c79581fe57df8ceb967cfa688
justinc <justinc@buildingiq.com>
2021-03-13 19:45:12 +1100

    Fix up build to export modules

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Global module import and export
+ */
+export { DirectivesLoader } from './airworthiness/directives-loader';
+export { ADType } from './airworthiness/directive-type';
+export { AircraftDirectiveData } from './airworthiness/aircraft-directive-data';
